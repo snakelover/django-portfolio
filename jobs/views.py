b8754f52d9cc29680aa854f1a50203d89e6228f0
Savva Morozov <pythontheback@gmail.com>
2021-09-14 17:00:02 -0300

    Change the name of the rendering function

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def homepage(request):
     return render(request, 'jobs/home.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home')
+    path('', jobs.views.homepage, name='home')
 ]
