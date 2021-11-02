26dfee31a403d2c488145716efcb1a52f0238dbd
Savva Morozov <pythontheback@gmail.com>
2021-11-02 16:38:54 -0300

    Cahnge the way of setting ALLOWED_HOSTS variable

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
